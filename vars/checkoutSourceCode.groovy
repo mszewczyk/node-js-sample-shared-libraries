9a38613ede7876f4f2b885d0a2f92145908e90d3
Marc Szewczyk <marc.szewczyk@gmail.com>
2019-01-13 16:11:43 -0500

    Moved GitCommit usage to source code step.

--- vars/checkoutSourceCode.groovy
+++ vars/checkoutSourceCode.groovy
@@ -1,7 +1,12 @@
 #!/usr/bin/env groovy
 
+import com.dminc.GitCommit
+
 def call(Map args) {
   stage('Checkout source code') {
-      checkout scm
+    checkout scm
+    String commitId = sh(script: 'git rev-parse HEAD', returnStdout: true)
+    GitCommit gitCommit = new GitCommit(id: commitId)
+    return gitCommit
   }
 }
