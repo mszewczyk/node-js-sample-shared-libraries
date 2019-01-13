8096e67f5248c9b044cda11308ff5bd7a4035902
Marc Szewczyk <marc.szewczyk@gmail.com>
2019-01-13 16:25:59 -0500

    Force push to Heroku.

--- src/com/dminc/Heroku.groovy
+++ src/com/dminc/Heroku.groovy
@@ -23,7 +23,7 @@ class Heroku {
 
   private void pushToHerokuGitRepo() {
     withCredentials([usernamePassword(credentialsId: 'heroku', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-      sh "git push https://${USERNAME}:${PASSWORD}@git.heroku.com/${appName}.git ${pipeline.env.TAG_NAME}:master"
+      sh "git push -f https://${USERNAME}:${PASSWORD}@git.heroku.com/${appName}.git ${pipeline.env.TAG_NAME}:master"
     }
   }
 }
